bf6aaf94d0e6e4b91b7d8b6339f44ff4050a61ba
p1tag0ras <franklinlugo2805@gmail.com>
2015-11-18 12:19:17 -0430

    testing img main

--- script/js/js.js
+++ script/js/js.js
@@ -1,8 +1,10 @@
-function testing(objeto) {
-
-	var foo = jQuery(objeto).find("p a").text();
-	// var foo = $("p a", this).text();
+// funcion que toma el parrafo del menu
+function FadeInMsg(object) {
+	// var foo = jQuery(objeto).find("p").text();
+	var foo = jQuery("p", object).text();
 	console.log(foo);
+	jQuery(object).find("p").fadeIn(1000);
+	jQuery(object).find("p").fadeOut(1000);
 }
 jQuery(document).ready(function(){
 });
