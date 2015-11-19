dacc91fd8d08bd30c10f78dcdd748a32ee7e5e8b
p1tag0ras <franklinlugo2805@gmail.com>
2015-11-19 10:23:29 -0430

    fix show hide msg menu item

--- script/js/js.js
+++ script/js/js.js
@@ -1,10 +1,8 @@
 // funcion que aplica fadeIn al mensaje de cada item del menu
 function FadeInMsg(object) {
-	// var foo = jQuery(objeto).find("p").text();
-	var foo = jQuery("p", object).text();
-	console.log(foo);
-	jQuery(object).find("p").fadeIn(1000);
-	jQuery(object).find("p").fadeOut(1000);
+	jQuery("p").hide();
+	// jQuery(object).find("p").fadeIn(1000);
+	jQuery("p", object).fadeIn(1000);
 }
 jQuery(document).ready(function(){
 });
