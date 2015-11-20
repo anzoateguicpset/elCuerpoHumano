3f990b65af37c21ce4d624036a37575355275b2e
p1tag0ras <franklinlugo2805@gmail.com>
2015-11-20 12:02:33 -0430

    afectos parts.html

--- script/js/js.js
+++ script/js/js.js
@@ -8,23 +8,22 @@ jQuery(document).ready(function(){
 
 	/* Parts.html */
 	jQuery("#parts__list--itemCabeza").mouseenter(function(){
-		console.log("entro mouse");
+		jQuery("#parts__section--itemMain").fadeToggle();
+		jQuery("#parts__section--itemCabeza").fadeToggle();
 		})
 	jQuery("#parts__list--itemCabeza").mouseleave(function(){
-		console.log("salio mouse");
+		jQuery("#parts__section--itemMain").fadeIn("fast");
+		jQuery("#parts__section--itemCabeza").fadeOut("fast");
 	})
 
 	jQuery("#parts__list--itemTronco").mouseenter(function(){
-		console.log("in funcion");
+		jQuery("#parts__section--itemMain").addClass("hidden");
+		jQuery("#parts__section--itemTronco").fadeIn(1000);
 		})
 
 	jQuery("#parts__list--itemExtre").mouseenter(function(){
-		console.log("in funcion");
+		jQuery("#parts__section--itemMain").addClass("hidden");
+		jQuery("#parts__section--itemExtre").fadeIn(1000);
 		})
-
-
-
-	
-	
 	
 });
