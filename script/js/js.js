74e9e1b9cb8ce1d7dca5283e2ff474a0826a6fa8
p1tag0ras <franklinlugo2805@gmail.com>
2015-11-20 11:29:58 -0430

    inicio efectos parts.html

--- script/js/js.js
+++ script/js/js.js
@@ -5,4 +5,26 @@ function FadeInMsg(object) {
 	jQuery("p", object).fadeIn(1000);
 }
 jQuery(document).ready(function(){
+
+	/* Parts.html */
+	jQuery("#parts__list--itemCabeza").mouseenter(function(){
+		console.log("entro mouse");
+		})
+	jQuery("#parts__list--itemCabeza").mouseleave(function(){
+		console.log("salio mouse");
+	})
+
+	jQuery("#parts__list--itemTronco").mouseenter(function(){
+		console.log("in funcion");
+		})
+
+	jQuery("#parts__list--itemExtre").mouseenter(function(){
+		console.log("in funcion");
+		})
+
+
+
+	
+	
+	
 });
