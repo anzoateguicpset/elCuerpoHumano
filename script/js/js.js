2e9d1fa1de78d50f919bd5bda179f69971f1ec73
p1tag0ras <franklinlugo2805@gmail.com>
2015-11-23 09:53:15 -0430

    efectos jquery parts.html

--- script/js/js.js
+++ script/js/js.js
@@ -7,23 +7,35 @@ function FadeInMsg(object) {
 jQuery(document).ready(function(){
 
 	/* Parts.html */
+	// cabeza
 	jQuery("#parts__list--itemCabeza").mouseenter(function(){
-		jQuery("#parts__section--itemMain").fadeToggle();
-		jQuery("#parts__section--itemCabeza").fadeToggle();
+		jQuery("#parts__section--itemMain").fadeToggle("slow");
+		jQuery("#parts__section--itemCabeza").fadeToggle("slow");
 		})
 	jQuery("#parts__list--itemCabeza").mouseleave(function(){
-		jQuery("#parts__section--itemMain").fadeIn("fast");
-		jQuery("#parts__section--itemCabeza").fadeOut("fast");
+		jQuery("#parts__section--itemMain").fadeToggle("slow");
+		jQuery("#parts__section--itemCabeza").fadeToggle("slow");
 	})
 
+	// Tronco
 	jQuery("#parts__list--itemTronco").mouseenter(function(){
-		jQuery("#parts__section--itemMain").addClass("hidden");
-		jQuery("#parts__section--itemTronco").fadeIn(1000);
+		jQuery("#parts__section--itemMain").fadeToggle("slow");
+		jQuery("#parts__section--itemTronco").fadeToggle("slow");
+		})
+	jQuery("#parts__list--itemTronco").mouseleave(function(){
+		jQuery("#parts__section--itemMain").fadeToggle("slow");
+		jQuery("#parts__section--itemTronco").fadeToggle("slow");
 		})
 
+	// Extremidades
 	jQuery("#parts__list--itemExtre").mouseenter(function(){
-		jQuery("#parts__section--itemMain").addClass("hidden");
-		jQuery("#parts__section--itemExtre").fadeIn(1000);
+		jQuery("#parts__section--itemMain").fadeToggle("slow");
+		jQuery("#parts__section--itemExtre").fadeToggle("slow");
 		})
+	jQuery("#parts__list--itemExtre").mouseleave(function(){
+		jQuery("#parts__section--itemMain").fadeToggle("slow");
+		jQuery("#parts__section--itemExtre").fadeToggle("slow");
+		})
+	/* Fin Parts.html*/
 	
 });
