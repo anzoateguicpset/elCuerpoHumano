c05c0f11163deaade8651367cbd497fe2acae292
p1tag0ras <franklinlugo2805@gmail.com>
2015-11-23 11:31:16 -0430

    contenido inside.html y health.html

--- script/js/js.js
+++ script/js/js.js
@@ -9,33 +9,69 @@ jQuery(document).ready(function(){
 	/* Parts.html */
 	// cabeza
 	jQuery("#parts__list--itemCabeza").mouseenter(function(){
-		jQuery("#parts__section--itemMain").fadeToggle("slow");
-		jQuery("#parts__section--itemCabeza").fadeToggle("slow");
+		jQuery("#parts__section--itemMain").fadeToggle("fast");
+		jQuery("#parts__section--itemCabeza").fadeToggle("fast");
 		})
 	jQuery("#parts__list--itemCabeza").mouseleave(function(){
-		jQuery("#parts__section--itemMain").fadeToggle("slow");
-		jQuery("#parts__section--itemCabeza").fadeToggle("slow");
+		jQuery("#parts__section--itemMain").fadeToggle("fast");
+		jQuery("#parts__section--itemCabeza").fadeToggle("fast");
 	})
 
 	// Tronco
 	jQuery("#parts__list--itemTronco").mouseenter(function(){
-		jQuery("#parts__section--itemMain").fadeToggle("slow");
-		jQuery("#parts__section--itemTronco").fadeToggle("slow");
+		jQuery("#parts__section--itemMain").fadeToggle("fast");
+		jQuery("#parts__section--itemTronco").fadeToggle("fast");
 		})
 	jQuery("#parts__list--itemTronco").mouseleave(function(){
-		jQuery("#parts__section--itemMain").fadeToggle("slow");
-		jQuery("#parts__section--itemTronco").fadeToggle("slow");
+		jQuery("#parts__section--itemMain").fadeToggle("fast");
+		jQuery("#parts__section--itemTronco").fadeToggle("fast");
 		})
 
 	// Extremidades
 	jQuery("#parts__list--itemExtre").mouseenter(function(){
-		jQuery("#parts__section--itemMain").fadeToggle("slow");
-		jQuery("#parts__section--itemExtre").fadeToggle("slow");
+		jQuery("#parts__section--itemMain").fadeToggle("fast");
+		jQuery("#parts__section--itemExtre").fadeToggle("fast");
 		})
 	jQuery("#parts__list--itemExtre").mouseleave(function(){
-		jQuery("#parts__section--itemMain").fadeToggle("slow");
-		jQuery("#parts__section--itemExtre").fadeToggle("slow");
+		jQuery("#parts__section--itemMain").fadeToggle("fast");
+		jQuery("#parts__section--itemExtre").fadeToggle("fast");
 		})
 	/* Fin Parts.html*/
 	
+
+	/*inside.html*/
+	//musculos
+	jQuery("#inside__list--itemMusculos").mouseenter(function(){
+		jQuery("#inside__section--itemMain").fadeToggle("fast");
+		jQuery("#inside__section--itemMusculos").fadeToggle("fast");
+		})
+	jQuery("#inside__list--itemMusculos").mouseleave(function(){
+		jQuery("#inside__section--itemMain").fadeToggle("fast");
+		jQuery("#inside__section--itemMusculos").fadeToggle("fast");
+	})
+	//Esqueleto
+	jQuery("#inside__list--itemEsqueleto").mouseenter(function(){
+		jQuery("#inside__section--itemMain").fadeToggle("fast");
+		jQuery("#inside__section--itemEsqueleto").fadeToggle("fast");
+		})
+	jQuery("#inside__list--itemEsqueleto").mouseleave(function(){
+		jQuery("#inside__section--itemMain").fadeToggle("fast");
+		jQuery("#inside__section--itemEsqueleto").fadeToggle("fast");
+	})
+	//cuidados
+	jQuery("#inside__list--itemCuidado").mouseenter(function(){
+		jQuery("#inside__section--itemMain").fadeToggle("fast");
+		jQuery("#inside__section--itemCuidados").fadeToggle("fast");
+		})
+	jQuery("#inside__list--itemCuidado").mouseleave(function(){
+		jQuery("#inside__section--itemMain").fadeToggle("fast");
+		jQuery("#inside__section--itemCuidados").fadeToggle("fast");
+	})
+	/*fin inside.html*/
+
+	/*health.html*/
+	
+	/*fin health.html*/
+
+
 });
