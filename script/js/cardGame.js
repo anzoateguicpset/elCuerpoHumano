e07a865cdc3e3480754483c9044b86576bdb15a9
p1tag0ras <franklinlugo2805@gmail.com>
2016-01-07 08:39:04 -0430

    initial file for cardGame

---
+++ script/js/cardGame.js
@@ -0,0 +1,90 @@
+/* cardGame.js */
+
+var correctCard = 0;
+$(init);
+
+function init(){
+	$('#successMessage').hide();
+
+
+	// reset the game
+	correctCards = 0;
+	$('#cardPile').html('');
+	$('#cardSlots').html('');
+
+
+	// create the pile of shuffled cards
+	var numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+	numbers.sort(function(){
+		return Math.random() - .5
+	});
+
+
+	for (var i = 0; i < 10; i++) {
+		$('<div>' +numbers[i]+'</div>').data('number', numbers[i]).attr('id','card'+numbers[i]).appendTo('#cardPile').draggable({
+			containment: '#content',
+			stack: '#cardPile div',
+			cursor: 'move',
+			revert: true
+		});
+	}
+
+
+	// create the card slots
+	var words = ['uno', 'dos', 'tres', 'cuatro', 'cinco', 'seis', 'siete', 'ocho', 'nueve', 'diez'];
+	for (var i = 1; i <= 10  ; i++) {
+		$('<div>'+words[i-1]+'</div>').data('number', i).appendTo('#cardSlots').droppable({
+			accept: '#cardPile div',
+			hoverClass: 'hovered',
+			drop: handleCardDrop
+		});
+	}
+
+}
+
+function handleCardDrop(event, ui){
+
+	var slotNumber = $(this).data('number');
+	var cardNumber = ui.draggable.data('number');
+
+
+	/*
+
+	If the card was dropped to the correct slot,
+	change the card colour, position it directly
+	on top of the slot, and prevent it being dragged again
+
+	*/
+
+	if(slotNumber == cardNumber){
+		ui.draggable.addClass('correct');
+		ui.draggable.draggable('disable');
+		$(this).droppable('disable');
+		ui.draggable.position({
+			of: $(this),
+			my: 'left top',
+			at: 'left top'
+		});
+		ui.draggable.draggable('option', 'revert', false);
+		correctCards++;
+	}
+
+	
+	/*
+
+	If all the cards have been placed correctly then display a message
+	 and reset the cards for another go
+
+	*/
+
+	if(correctCards == 10){
+		$('#successMessage').show();
+		$('#successMessage').animate({
+			left: '380px',
+			top: '200px',
+			width: '400px',
+			height: '100px',
+			opacity: 1
+		});
+	}
+}
