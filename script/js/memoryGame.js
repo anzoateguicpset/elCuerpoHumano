5fafba58c936ac6ff6a811553116f9ed79597da1
p1tag0ras <franklinlugo2805@gmail.com>
2016-01-26 14:26:17 -0430

    fix path img memoryGame

--- script/js/memoryGame.js
+++ script/js/memoryGame.js
@@ -5,19 +5,6 @@ var ImgFound = 0;
 
 var Source = "#boxcard";
 
-// var ImgSource = [
-//   "http://img5.uploadhouse.com/fileuploads/17699/176992640c06707c66a5c0b08a2549c69745dc2c.png",
-//   "http://img6.uploadhouse.com/fileuploads/17699/17699263b01721074bf094aa3bc695aa19c8d573.png",
-//   "http://img6.uploadhouse.com/fileuploads/17699/17699262833250fa3063b708c41042005fda437d.png",
-//   "http://img9.uploadhouse.com/fileuploads/17699/176992615db99bb0fd652a2e6041388b2839a634.png",
-//   "http://img4.uploadhouse.com/fileuploads/17699/176992601ca0f28ba4a8f7b41f99ee026d7aaed8.png",
-//   "http://img3.uploadhouse.com/fileuploads/17699/17699259cb2d70c6882adc285ab8d519658b5dd7.png",
-//   "http://img2.uploadhouse.com/fileuploads/17699/1769925824ea93cbb77ba9e95c1a4cec7f89b80c.png",
-//   "http://img7.uploadhouse.com/fileuploads/17699/1769925708af4fb3c954b1d856da1f4d4dcd548a.png",
-//   "http://img9.uploadhouse.com/fileuploads/17699/176992568b759acd78f7cbe98b6e4a7baa90e717.png",
-//   "http://img9.uploadhouse.com/fileuploads/17699/176992554c2ca340cc2ea8c0606ecd320824756e.png"
-// ];
-
 var ImgSource = [
 	"../imagenes/memoryGame/001.png",
 	"../imagenes/memoryGame/002.png",
