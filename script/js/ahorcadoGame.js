944c6d965fba1c5142c01e460d376bba0071ebe5
p1tag0ras <franklinlugo2805@gmail.com>
2016-01-28 12:36:51 -0430

    actividad dentro del cuerpo humano and fix some errors if js

--- script/js/ahorcadoGame.js
+++ script/js/ahorcadoGame.js
@@ -1,12 +1,19 @@
+function resetAhorcado(){
+    // $("#inside__success-js").hide();
+    location.href="inside_activities.html";
+
+}
+
 $(document).ready(function() {
     var respuesta = new Array();
     var errores = 1;
+    $("#inside__success-js").hide();
 
     ////////////////////////////////////////////////
     // botones creados dinamicamente usando ASCII //
     ////////////////////////////////////////////////
     for (var i = 65; i < 91; i++) {
-        $('#letras').append('<button class="btn  btn-default" id="letra'+String.fromCharCode(i)+'">'+String.fromCharCode(i)+'</button>');
+        $('#letras-container-js').append('<button class="btn  btn-default" id="letra'+String.fromCharCode(i)+'">'+String.fromCharCode(i)+'</button>');
     };
 
     ///////////////////////
@@ -14,29 +21,34 @@ $(document).ready(function() {
     ///////////////////////
     frases = {
         0:"los musculos permiten correr reir agarrar y saltar",
-        1:"las proteinas de origen animal es la mejor para los musculos",
+        1:"las proteinas de origen animal son la mejores para los musculos",
         2:"los huesos se componen de calcio fosforo y colageno",
         3:"la estructura del cuerpo esta conformada por los huesos",
         4:"hacer ejercicio favorece el aumento de la masa muscular y de la fortaleza osea",
-        5:"el cuerpo humano de un adulto tiene doscientos seis huesos"
+        5:"el cuerpo humano de un adulto tiene doscientos seis huesos",
     };
 
 
     ///////////////////////
     // Todo a mayusculas //
     ///////////////////////
-    for (var i = 0; i < 5; i++) {
+    for (var i = 0; i <= 5; i++) {
       frases[i] = frases[i].toUpperCase();
     };
 
   //////////////////////////////////////////
   // numero al azar para elegir una frase //
   //////////////////////////////////////////
-  var azar = Math.floor((Math.random() * 5) + 1);
+  var azar = Math.floor((Math.random() * 6));
+  console.log(azar);
+  console.log(frases[azar]);
+  // for (var i = 0; i <= 5; i++) {
+  //     console.log(Math.floor((Math.random() * 6)));
+  // };
   for (var i = 0; i < frases[azar].length; i++) {
     if(frases[azar].charAt(i) != ' ')respuesta[i] = '_ ';
     else respuesta[i] = '\n'
-        $('#secreto').append(respuesta[i]);
+        $('#secreto-js').append(respuesta[i]);
   };
 
 /////////////////////////////////////////////////
@@ -46,14 +58,14 @@ $('.btn-default').click(function(event) {
     // Sacamos la tecla que se pulso
     var id= this.id.charAt(5);
     var ban = false;
-    $('#secreto').empty();
+    $('#secreto-js').empty();
     // Comparamos si la letra que se eligio esta en la frase y se va agregado
     for (var i = 0; i < frases[azar].length; i++) {
         if(frases[azar].charAt(i) == id){
             respuesta[i] = id;
             ban = true;
         }
-        $('#secreto').append(respuesta[i]);
+        $('#secreto-js').append(respuesta[i]);
     };
 
     // Si la letra no esta en la frase se aumentan los errores y se cambia la imagen
@@ -78,8 +90,7 @@ $('.btn-default').click(function(event) {
             }
         };
         if(ban2){
-            alert('¡Felicidades! :)');
-            location.href="inside_activities.html";
+            $("#inside__success-js").fadeIn("fast");
 
         }
     }
