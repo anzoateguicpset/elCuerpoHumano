b8790486298c1ff1c1ce703767936671a05affa3
p1tag0ras <franklinlugo2805@gmail.com>
2016-02-01 12:01:46 -0430

    set filenames

---
+++ script/js/ahorcado-game.js
@@ -0,0 +1,102 @@
+function resetAhorcado(){
+    // $("#inside__success-js").hide();
+    location.href="inside-activities.html";
+
+}
+
+$(document).ready(function() {
+    var respuesta = new Array();
+    var errores = 1;
+    $("#inside__success-js").hide();
+
+    ////////////////////////////////////////////////
+    // botones creados dinamicamente usando ASCII //
+    ////////////////////////////////////////////////
+    for (var i = 65; i < 91; i++) {
+        $('#letras-container-js').append('<button class="btn  btn-default" id="letra'+String.fromCharCode(i)+'">'+String.fromCharCode(i)+'</button>');
+    };
+
+    ///////////////////////
+    // Objeto de frases //
+    ///////////////////////
+    frases = {
+        0:"los musculos permiten correr reir agarrar y saltar",
+        1:"las proteinas de origen animal son la mejores para los musculos",
+        2:"los huesos se componen de calcio fosforo y colageno",
+        3:"la estructura del cuerpo esta conformada por los huesos",
+        4:"hacer ejercicio favorece el aumento de la masa muscular y de la fortaleza osea",
+        5:"el cuerpo humano de un adulto tiene doscientos seis huesos",
+    };
+
+
+    ///////////////////////
+    // Todo a mayusculas //
+    ///////////////////////
+    for (var i = 0; i <= 5; i++) {
+      frases[i] = frases[i].toUpperCase();
+    };
+
+  //////////////////////////////////////////
+  // numero al azar para elegir una frase //
+  //////////////////////////////////////////
+  var azar = Math.floor((Math.random() * 6));
+  console.log(azar);
+  console.log(frases[azar]);
+  // for (var i = 0; i <= 5; i++) {
+  //     console.log(Math.floor((Math.random() * 6)));
+  // };
+  for (var i = 0; i < frases[azar].length; i++) {
+    if(frases[azar].charAt(i) != ' ')respuesta[i] = '_ ';
+    else respuesta[i] = '\n'
+        $('#secreto-js').append(respuesta[i]);
+  };
+
+/////////////////////////////////////////////////
+// Cuando se hace click a una letra entra aqui //
+/////////////////////////////////////////////////
+$('.btn-default').click(function(event) {
+    // Sacamos la tecla que se pulso
+    var id= this.id.charAt(5);
+    var ban = false;
+    $('#secreto-js').empty();
+    // Comparamos si la letra que se eligio esta en la frase y se va agregado
+    for (var i = 0; i < frases[azar].length; i++) {
+        if(frases[azar].charAt(i) == id){
+            respuesta[i] = id;
+            ban = true;
+        }
+        $('#secreto-js').append(respuesta[i]);
+    };
+
+    // Si la letra no esta en la frase se aumentan los errores y se cambia la imagen
+    // Al alcanzar los cinco errores el juego termina
+    if(!ban){
+        errores++;
+        $('#imagen').attr('src', 'img/a'+errores+'.jpg');
+        $('#imagen').attr('src', '../imagenes/ahorcado-game/a'+errores+'.jpg');
+        if(errores == 5){
+            alert('Vuelve a intentarlo. La frase era: '+frases[azar]);
+            location.href="inside-activities.html";
+        }
+    }
+    
+    // Si la letra esta en la frase se verifica si se completo la frase
+    else{
+        var ban2 = true;
+        for (var i = 0; i < respuesta.length; i++) {
+            if(respuesta[i] == '_ '){
+                ban2 = false;
+                break;
+            }
+        };
+        if(ban2){
+            $("#inside__success-js").fadeIn("fast");
+
+        }
+    }
+    // Se desactiva la letra que ya se pulso
+    $(this).removeClass('btn-default');
+    $(this).attr('disabled','disabled');
+});
+});
+
---
+++ script/js/card-game.js
@@ -0,0 +1,105 @@
+/* cardGame.js */
+
+var correctCard = 0;
+$(init);
+
+/**
+ * Inicia la ejecucion del juego
+ */
+function init(){
+	$('#successMessage').hide();
+
+
+	////////////////////
+	// reset del juego //
+	////////////////////
+	correctCards = 0;
+	$('#cardPile').html('');
+	$('#cardSlots').html('');
+
+
+	/////////////////////////
+	// objeto de imagenes //
+	/////////////////////////
+
+	var imagenes = {
+		1:"<img src='../imagenes/card-game/brazo.png'>",
+		2:"<img src='../imagenes/card-game/cabeza.png'>",
+		3:"<img src='../imagenes/card-game/pie.png'>",
+		4:"<img src='../imagenes/card-game/pierna.png'>",
+		5:"<img src='../imagenes/card-game/tronco.png'>"
+	};
+
+
+	// // desordenando el arreglo de las imagenes
+	// imagenes.sort(function(){
+	// 	return Math.random() - .5
+	// });
+
+	console.log(imagenes);
+
+
+	// Haciendo draggable cada imagen
+	for (var i = 5; i > 0; i--) {
+		$('<div>' +imagenes[i]+'</div>').data('number', i).attr('id','card'+i).appendTo('#cardPile').draggable({
+			containment: '#content',
+			stack: '#cardPile div',
+			cursor: 'move',
+			revert: true
+		});
+	}
+
+	/////////////////////////////
+	// Slots para las imagenes //
+	/////////////////////////////
+	var words = ['Brazo', 'Cabeza', 'Pie', 'Pierna', 'Tronco'];
+		for (var i = 0; i < 5  ; i++) {
+		$('<div>'+words[i]+'</div>').data('number', i+1).appendTo('#cardSlots').droppable({
+			accept: '#cardPile div',
+			hoverClass: 'hovered',
+			drop: handleCardDrop
+		});
+	}
+
+}
+
+/**
+ * [Si la imagen fue arrastrada al slot correcto,
+ * cambia el color de la imagen, y se posiciona directamente
+ * arriba del slot previniendo que sea arrastrada otra vez]
+ * @param  {[Objeto]} event [Evento de la pagina]
+ * @param  {[Objeto]} ui    [description]
+ */
+function handleCardDrop(event, ui){
+
+	var slotNumber = $(this).data('number');
+	var cardNumber = ui.draggable.data('number');
+
+	console.log("numero de slot "+slotNumber);
+	console.log("numero de carta "+cardNumber);
+
+	if(slotNumber == cardNumber){
+		ui.draggable.addClass('correct');
+		ui.draggable.draggable('disable');
+		$(this).droppable('disable');
+		ui.draggable.position({
+			of: $(this),
+			my: 'left top',
+			at: 'left top'
+		});
+		ui.draggable.draggable('option', 'revert', false);
+		correctCards++;
+	}
+
+	/**
+	 * Si todas las imagenes han sido colocadas en el slot correcto
+	 * se muestra el mensaje y se resetean las cartas para otro juego
+	 */
+	if(correctCards == 5){
+		$('#successMessage').show();
+		$('#successMessage').animate({
+			top: '200px',
+			opacity: 1
+		}, 500);
+	}
+}
